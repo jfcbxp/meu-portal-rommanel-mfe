228fc80a762916320e1fe3da5287499fd8881e74
Joao Felipe <jfcbxp@gmail.com>
2025-05-29 14:33:42 -0300

    feat: 🎸 add PARTIALLY_PAID status (#18)

--- src/components/labels/statusChip/styles.tsx
+++ src/components/labels/statusChip/styles.tsx
@@ -6,8 +6,15 @@ export const StyledChip = styled.div<{ status: string }>`
   height: 20px;
   padding: 2px 8px;
   border-radius: ${({ theme }) => theme.borderRadius.small} !important;
-  color: ${({ theme, status }) =>
-    status === 'Pago' ? theme.colors.success : theme.colors.textLight};
+  color: ${({ theme, status }) => {
+    if (status === 'Pago Parcialmente') {
+      return theme.colors.infoText;
+    }
+    if (status === 'Pago') {
+      return theme.colors.success;
+    }
+    return theme.colors.textLight;
+  }};
 
   .p-chip-label {
     line-height: 1.2;
