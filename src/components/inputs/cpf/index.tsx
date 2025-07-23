fccf4c7090c50ece06dfc2b5893f5a761716d005
jfcbxp <jfcbxp@gmail.com>
2025-07-23 00:10:57 -0300

    refactor: 💡 upd login page

--- src/components/inputs/cpf/index.tsx
+++ src/components/inputs/cpf/index.tsx
@@ -6,12 +6,29 @@ import {
   StyledInputMask,
 } from './styles';
 
-export default function CPF(properties: Readonly<InputMaskProps>) {
-  const rawCpf = properties.value.replace(/\D/g, '');
-  const charCount = rawCpf.length;
+export default function Cgc(properties: Readonly<InputMaskProps>) {
+  const rawCgc = (properties.value || '').replace(/\D/g, '');
+  const charCount = rawCgc.length;
+
+  const getMaskAndPlaceholder = (value: string) => {
+    if (value.length > 11) {
+      return {
+        mask: '99.999.999/9999-99',
+        placeholder: '11.111.111/1111-11',
+        maxLength: 14,
+      };
+    }
+    return {
+      mask: '999.999.999-999  ',
+      placeholder: '111.111.111-11',
+      maxLength: 11,
+    };
+  };
+
+  const { mask, placeholder, maxLength } = getMaskAndPlaceholder(rawCgc);
 
   const handleOnFocus = (event: React.FocusEvent<HTMLInputElement>) => {
-    if (!rawCpf) {
+    if (!rawCgc) {
       event.target.setSelectionRange(0, 0);
     }
   };
@@ -19,18 +36,18 @@ export default function CPF(properties: Readonly<InputMaskProps>) {
   const handleOnClick = (event: React.MouseEvent<HTMLInputElement>) => {
     const input = event.target as HTMLInputElement;
 
-    if (document.activeElement === input && !rawCpf) {
+    if (document.activeElement === input && !rawCgc) {
       input.setSelectionRange(0, 0);
     }
   };
 
   return (
     <Container>
-      <Label htmlFor="cpf">CPF</Label>
+      <Label htmlFor="cpf">CPF/CNPJ</Label>
       <StyledInputMask
         {...properties}
-        mask="999.999.999-99"
-        placeholder="111.111.111-11"
+        mask={mask}
+        placeholder={placeholder}
         onFocus={handleOnFocus}
         onClick={handleOnClick}
         required
@@ -39,7 +56,9 @@ export default function CPF(properties: Readonly<InputMaskProps>) {
       />
       <HelperTextContainer>
         <span>Campo obrigatório</span>
-        <span>{charCount}/11</span>
+        <span>
+          {charCount}/{maxLength}
+        </span>
       </HelperTextContainer>
     </Container>
   );
